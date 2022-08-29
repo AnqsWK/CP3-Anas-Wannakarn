bf4ea06a3c6b6edf1836147b9e3e387d9222ca0e
AnqsWK <110518663+AnqsWK@users.noreply.github.com>
2022-08-29 20:55:09 +0700

    Delete exerxise4_Anas_W.py

--- exerxise4_Anas_W.py
+++
@@ -1,9 +0,0 @@
-FE = 80.5
-GB = 75.6
-ICS = 79.0
-CP = 93.4
-print("---Your Score---")
-print("Foundation English:",FE)
-print("General Business:",GB)
-print("Introduction to Computer Systems:",ICS)
-print("Computer Programming:",CP)
